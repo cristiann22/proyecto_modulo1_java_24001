9808498769670821e9f95b0caf67bfe0c13d3adc
cristiann22 <cristianmartinezz189@gmail.com>
2024-05-20 23:53:42 -0600

    cambios en login-js

--- js/login.js
+++ js/login.js
@@ -1,31 +1,30 @@
 const $submit = document.getElementById("submit");
-    $password = document.getElementById("password");
-    $email = document.getElementById("email");
-    $visible = document.getElementById("visible");
+    const $password = document.getElementById("password");
+    const $email = document.getElementById("email");
+    const $visible = document.getElementById("visible");
     
-    document.addEventListener("change", (e)=>{
-        if(e.target === $visible){
-            if($visible.checked === false) $password.type = "password";
-            else $password.type = "text";
-        }
-    });
+    $visible.addEventListener("change", (e)=>{
+            // if($visible.checked === false) $password.type = "password";
+            // else $password.type = "text";
+            $password.type = ($visible.checked === false) ? "password" : "text"
+        });
     
 const $submitSign = document.getElementById("submitSign")
-    $passwordSign = document.getElementById("passwordSign")
-    $visibleSign = document.getElementById("visibleSign")
+    const $passwordSign = document.getElementById("passwordSign")
+    const $visibleSign = document.getElementById("visibleSign")
     
-    document.addEventListener("change", (e)=>{
-        if(e.target === $visibleSign) {
-            if($visibleSign.checked === false) $passwordSign.type = "passwordSign";
-            else $passwordSign.type = "text";
-        }
+    $visibleSign.addEventListener("change", (e)=>{
+        // if(e.target === $visibleSign) {
+        //     if($visibleSign.checked === false) $passwordSign.type = "passwordSign";
+        //     else $passwordSign.type = "text";
+        // }
+        $passwordSign.type = ($visibleSign.checked === false) ? "password" : "text"
     });
 
-    document.addEventListener("click", (e)=>{
-        if (e.target === $submit) {
+    $submit.addEventListener("click", (e)=>{
+        e.preventDefault();
+        e.stopPropagation();
             if($password.value !== "" && $email.value !== "") {
-                e.preventDefault();
                 window.location.href = "inicio.html";
             }
-        }
     })
